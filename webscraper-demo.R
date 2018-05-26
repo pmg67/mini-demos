24d386c9282d1921bf551913dfd2335e1ada4003
Patrick M Groden <pmg67@uw.edu>
2018-05-25 19:36:47 -0700

    updated with webscraper exercise

--- webscraper-demo.R
+++ webscraper-demo.R
@@ -27,71 +27,78 @@ rank_data<-as.numeric(rank_data)
 head(rank_data)
 
 #Using CSS selectors to scrape the title section
+title_data <- html_nodes(webpage,'.lister-item-header a')
 
 #html to text 
+title_data_text <- html_text(title_data)
 
 #look at data
+head(title_data_text)
 
 #Using CSS selectors to scrape the description section
+description_data <- html_nodes(webpage,'.ratings-bar+ .text-muted')
 
 #Converting the description data to text
+description_data_text <- html_text(description_data)
 
 #look at data
+head(description_data_text)
 
 #Data-Preprocessing: removing '\n'
+description_data_text <-gsub("\n","",description_data_text)
 
 #Using CSS selectors to scrap the Movie runtime section
 
+runtime_data <- html_nodes(webpage,'.text-muted .runtime')
+
 #Converting the movie runtime data to text
+runtime_data_text <- html_text(runtime_data)
 
 #Let's have a look at the movie runtime
+head(runtime_data_text)
 
 #Data-Preprocessing: removing mins and converting it to numerical
 
+runtime_data_text <- gsub(" min","",runtime_data_text)
+runtime_data_text <- as.numeric(runtime_data_text)
 
 #Let's have another look at the runtime data
+head(rank_data)
 
+genre_data <- html_nodes(webpage,'.genre')
 
 #Converting the genre data to text
+genre_data_text <- html_text(genre_data)
 
 #Let's have a look at the genre
+head(genre_data_text)
 
 #Data-Preprocessing: removing \n
+genre_data_text <- gsub("\n","",genre_data_text)
 
 #Data-Preprocessing: removing excess spaces
+genre_data_text <- gsub(" ","",genre_data_text)
 
 #taking only the first genre of each movie
+genre_data_text <- gsub(",.*","",genre_data_text)
 
 #Convering each genre from text to factor
+genre_data_text <- as.factor(genre_data_text)
 
 #Let's have another look at the genre data
+head(genre_data_text)
 
 #Using CSS selectors to scrap the IMDB rating section
+rating_data <- html_nodes(webpage,'.ratings-imdb-rating strong')
 
 #Converting the ratings data to text
+rating_data_text <- html_text(rating_data)
 
 #Let's have a look at the ratings
+head(rating_data_text)
 
 #Data-Preprocessing: converting ratings to numerical
-
-#Let's have another look at the ratings data
-
-
-#Using CSS selectors to scrap the directors section
-
-#Converting the directors data to text
-
-#Let's have a look at the directors data
-
-#Data-Preprocessing: converting directors data into factors
-
-#Using CSS selectors to scrap the actors section
-
-#Converting the gross actors data to text
-
-#Let's have a look at the actors data
-
-#Data-Preprocessing: converting actors data into factors
+rating_data<-as.numeric(rating_data_text)
 
 #Using CSS selectors to scrap the gross revenue section
 
